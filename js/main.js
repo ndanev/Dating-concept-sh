7ae1f00e03de771fc6b479e493a6a69978fba71f
Nemanja Danev <nemanjadanev@Nemanjas-Mac-mini.local>
2020-05-12 13:40:32 +0200

    changed header

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
-// window.onscroll = function () {
-//     bgHeader();
-// };
+window.onscroll = function () {
+    bgHeader();
+};
 
-// function bgHeader() {
-//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//         document.getElementById("header").style.backgroundColor = '#1f1f1f';
-//     } else {
-//         document.getElementById("header").style.backgroundColor = 'transparent';
-//     }
-// }
-
-
-
-
-
+function bgHeader() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("header").style.backgroundColor = '#1f1f1f';
+    } else {
+        document.getElementById("header").style.backgroundColor = 'transparent';
+    }
+}
 
 $(document).ready(function () {
-
     $('#navbar-btn').click(function () {
         if ($('#header')) {
             $('#header').css('background-color', '#1f1f1f');
@@ -95,32 +89,9 @@ $(document).ready(function () {
     });
 
 
-
-
     /* move overflow-y to the bottom */
     $('.single-chatbox-body').scrollTop($('.single-chatbox-body')[0].scrollHeight);
 
-});
-
-var inputs = document.querySelectorAll( '.input-file' );
-Array.prototype.forEach.call( inputs, function( input )
-{
-    var label	 = input.nextElementSibling,
-        labelVal = label.innerHTML;
-    console.log(input)
-	input.addEventListener( 'change', function( e )
-	{
-		var fileName = '';
-		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-		else
-			fileName = e.target.value.split( "\ " ).pop();
-
-		if( fileName )
-			label.querySelector( 'span' ).innerHTML = fileName;
-		else
-			label.innerHTML = labelVal;
-	});
 });
 
 
