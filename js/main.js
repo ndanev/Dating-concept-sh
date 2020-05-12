6f4951682fbaecebfa41cc493e41b41e4c85763e
Nemanja Danev <nemanjadanev@Nemanjas-Mac-mini.local>
2020-05-12 13:09:14 +0200

    added subscription-expired page and fixed footer spaces

--- js/main.js
+++ js/main.js
@@ -102,6 +102,28 @@ $(document).ready(function () {
 
 });
 
+var inputs = document.querySelectorAll( '.input-file' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+    var label	 = input.nextElementSibling,
+        labelVal = label.innerHTML;
+    console.log(input)
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( "\ " ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
+
+
 /* Drop image file */
 
 function dropImage() {
@@ -131,6 +153,7 @@ function dropImage() {
 }
 
 dropImage();
+
 
 
 
